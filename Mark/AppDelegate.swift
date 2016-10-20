4ab3b3174c90b73cf605bbcace08dcd4e4814b79
Velislava Yanchina <vel.yanchina@gmail.com>
2016-10-20 14:28:14 +0300

    Initial Commit

---
+++ Mark/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mark
+//
+//  Created by Velislava Yanchina on 10/20/16.
+//  Copyright © 2016 Velislava Yanchina. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mark/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mark
+//
+//  Created by Velislava Yanchina on 10/20/16.
+//  Copyright © 2016 Velislava Yanchina. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
