2279175603209a934e9d291fd2356ff4b18e27d0
Velislava Yanchina <vel.yanchina@gmail.com>
2016-10-20 14:32:24 +0300

    Created repo

---
+++ MarkExtension/SourceEditorCommand.swift
@@ -0,0 +1,20 @@
+//
+//  SourceEditorCommand.swift
+//  MarkExtension
+//
+//  Created by Velislava Yanchina on 10/20/16.
+//  Copyright © 2016 Velislava Yanchina. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
